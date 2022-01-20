ee821c8f024df80f88fcfc1a7050b11af91361a3
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-20 18:18:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -96,14 +96,6 @@ console.log(add_years(dt, 10).toString());
 // "Sun Jan 11 2015 00:00:00 GMT+0530 (India Standard Time)"
 
 
-
-
-
-
-
-
-
-
 function add_weeks(dt, n) 
  {
  return new Date(dt.setDate(dt.getDate() + (n * 7)));      
@@ -113,6 +105,7 @@ dt = new Date();
 console.log(add_weeks(dt, 10).toString());  
 dt = new Date(2014,10,2);
 console.log(add_weeks(dt, 10).toString());
+
 
 
 // Write a JavaScript function to add specified months to a date.   
